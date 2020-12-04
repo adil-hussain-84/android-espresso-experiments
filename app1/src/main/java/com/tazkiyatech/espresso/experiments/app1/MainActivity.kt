ed76644b04a0a4a9836ba968fbb0caa98aae0f8b
Adil Hussain <>
2020-12-04 10:05:43 +0000

    (app1) Added logic in `MainActivity` to display the number of times `onResume()` has been called.

--- app1/src/main/java/com/tazkiyatech/espresso/experiments/app1/MainActivity.kt
+++ app1/src/main/java/com/tazkiyatech/espresso/experiments/app1/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.tazkiyatech.espresso.experiments.app1
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        count++
+
+        val text = resources.getQuantityString(R.plurals.onResume_called_xx_times, count, count)
+
+        findViewById<TextView>(R.id.textView).text = text
     }
 }
