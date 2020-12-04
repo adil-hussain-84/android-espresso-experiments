baaf3cb2d52043c8ca11c96fa6fa8486dc0770ea
Adil Hussain <>
2020-12-04 10:20:03 +0000

    (Android) Added a test method in `MainActivityTest` which confirms that the text in `MainActivity.textView` is as expected after `MainActivity` is launched.

--- app1/src/androidTest/java/com/tazkiyatech/espresso/experiments/app1/MainActivityTest.kt
+++ app1/src/androidTest/java/com/tazkiyatech/espresso/experiments/app1/MainActivityTest.kt
@@ -1,20 +1,35 @@
 package com.tazkiyatech.espresso.experiments.app1
 
+import android.app.Application
+import android.content.res.Resources
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
+    private val resources: Resources
+        get() = ApplicationProvider.getApplicationContext<Application>().resources
+
+    private lateinit var activityScenario: ActivityScenario<MainActivity>
+
     @Test
-    fun useAppContext() {
+    fun text_after_activity_is_launched() {
+        // Given.
+        val expected = resources.getQuantityString(R.plurals.onResume_called_xx_times, 1, 1)
+
         // When.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        activityScenario = launch(MainActivity::class.java)
 
         // Then.
-        assertEquals("com.tazkiyatech.espresso.experiments.app1", appContext.packageName)
+        onView(withId(R.id.textView)).check(matches(withText(expected)))
     }
 }
