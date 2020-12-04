d7d9fe1d1ed5a91b77f252a20ecb0f8cdb76414d
Adil Hussain <>
2020-12-04 12:34:32 +0000

    (app1) Added tests in `MainActivityTest` which demonstrate how the `ActivityScenario.moveState(newState:)` method works.

--- app1/src/androidTest/java/com/tazkiyatech/espresso/experiments/app1/MainActivityTest.kt
+++ app1/src/androidTest/java/com/tazkiyatech/espresso/experiments/app1/MainActivityTest.kt
@@ -1,10 +1,8 @@
 package com.tazkiyatech.espresso.experiments.app1
 
-import android.app.Application
-import android.content.res.Resources
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ActivityScenario.launch
-import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -16,20 +14,40 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    private val resources: Resources
-        get() = ApplicationProvider.getApplicationContext<Application>().resources
-
     private lateinit var activityScenario: ActivityScenario<MainActivity>
 
     @Test
     fun text_after_activity_is_launched() {
-        // Given.
-        val expected = resources.getQuantityString(R.plurals.onResume_called_xx_times, 1, 1)
-
         // When.
         activityScenario = launch(MainActivity::class.java)
 
         // Then.
-        onView(withId(R.id.textView)).check(matches(withText(expected)))
+        onView(withId(R.id.textView)).check(matches(withText("onCreate,onStart,onResume")))
+    }
+
+    @Test
+    fun text_after_activity_is_launched_then_moved_back_to_created_state_then_moved_forward_to_resumed_state() {
+        // Given.
+        activityScenario = launch(MainActivity::class.java)
+
+        // When.
+        activityScenario.moveToState(Lifecycle.State.CREATED)
+        activityScenario.moveToState(Lifecycle.State.RESUMED)
+
+        // Then.
+        onView(withId(R.id.textView)).check(matches(withText("onCreate,onStart,onResume,onPause,onStop,onStart,onResume")))
+    }
+
+    @Test
+    fun text_after_activity_is_launched_then_moved_back_to_started_state_then_moved_forward_to_resumed_state() {
+        // Given.
+        activityScenario = launch(MainActivity::class.java)
+
+        // When.
+        activityScenario.moveToState(Lifecycle.State.STARTED)
+        activityScenario.moveToState(Lifecycle.State.RESUMED)
+
+        // Then.
+        onView(withId(R.id.textView)).check(matches(withText("onCreate,onStart,onResume,onPause,onResume")))
     }
 }
--- app1/src/main/java/com/tazkiyatech/espresso/experiments/app1/MainActivity.kt
+++ app1/src/main/java/com/tazkiyatech/espresso/experiments/app1/MainActivity.kt
@@ -1,25 +1,51 @@
 package com.tazkiyatech.espresso.experiments.app1
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var count = 0
+    lateinit var stringBuilder: StringBuilder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        stringBuilder = StringBuilder()
+
+        textView.text = appendString("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        textView.text = appendString("onStart")
     }
 
     override fun onResume() {
         super.onResume()
+        textView.text = appendString("onResume")
+    }
 
-        count++
+    override fun onPause() {
+        super.onPause()
+        textView.text = appendString("onPause")
+    }
 
-        val text = resources.getQuantityString(R.plurals.onResume_called_xx_times, count, count)
+    override fun onStop() {
+        super.onStop()
+        textView.text = appendString("onStop")
+    }
 
-        findViewById<TextView>(R.id.textView).text = text
+    override fun onDestroy() {
+        super.onDestroy()
+        textView.text = appendString("onDestroy")
+    }
+
+    private fun appendString(string: String): String {
+        if (stringBuilder.isNotEmpty()) {
+            stringBuilder.append(",")
+        }
+        return stringBuilder.append(string).toString()
     }
 }
